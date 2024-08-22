1c74cb0bc7b93e2b5d4141843b9a11c1846b1d7b
matheus-bicarato <mr.bicarato@outlook.com>
2024-08-22 12:49:24 -0300

    a

--- StudentsCool/front/StudentsCool/src/App.tsx
+++ StudentsCool/front/StudentsCool/src/App.tsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
-
 import './App.css'
+import { Outlet } from 'react-router-dom'
+import Header from './components/Header.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    
+    <div className="App">
+      <Header />
+
+      <Outlet />
+    </div>
   )
 }
 
