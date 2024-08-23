6de7305edebbabcbcd98ed04a4efe5ffdf184316
matheus-bicarato <mr.bicarato@outlook.com>
2024-08-23 09:31:00 -0300

    aaaaaaaa

---
+++ StudentsCool/front/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+import logo from '../assets/imagens/logo_lapis.png'
+import perfil from '../assets/imagens/Perfil.png'
+import porta from '../assets/imagens/Porta.png'
+import './styles/Header.css'
+
+const Header = () => {
+    return (
+        <header className='Navbar'>
+            <div className="logo">
+                <Link to={"/"}><img className="logo" src={logo} alt="logo" /></Link>
+            </div>
+            <div className="perfil">
+                <Link to={"/"}><img className='img-perfil' src={perfil} alt="perfil" /></Link>
+                <h2 className='user'>Rafaela Alonso dos Anjos <br /> <span className="subUser">STUDENTSCOOL</span></h2>
+                <Link to={"/"}><img className="porta" src={porta} alt="porta" /></Link>
+            </div>
+        </header>
+    )
+}
+export default Header;
--- StudentsCool/front/src/router/Home.jsx
+++ StudentsCool/front/src/router/Home.jsx
@@ -1,13 +1,11 @@
-import logo from '../assets/imagens/logo_pnc.png'
-const Home = () => {
-    return(
-        <div className="Navbar">
-            <div className="logo">
-                <img src={logo} alt="logo" />
-            </div>
-            <div className="botao_voltar">
 
-            </div>
+import Header from '../components/Header';
+import './styles/Home.css'
+
+const Home = () => {
+    return (
+        <div className="">
+            <Header/>
         </div>
     )
 }
