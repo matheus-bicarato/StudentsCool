3089845e36214be893a09ff065de329acd98f7d9
Fl0rzinha <gabriel.florindo.souza46@gmail.com>
2024-08-29 16:07:40 -0300

    Init Spring Boot
    Criando o projeto básico da api no spring boot (vazio por enquanto)

---
+++ back/src/main/java/com/studentscool/StudentsCool/StudentsCoolApplication.java
@@ -0,0 +1,13 @@
+package com.studentscool.StudentsCool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsCoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsCoolApplication.class, args);
+	}
+
+}
