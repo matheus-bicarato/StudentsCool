398a16cf969ed5d533e40a2adc2e7e401a6a60ed
matheus-bicarato <mr.bicarato@outlook.com>
2024-08-30 08:09:46 -0300

    cadastro

--- front/src/router/Cadastro.jsx
+++ front/src/router/Cadastro.jsx
@@ -1,10 +1,10 @@
-// import Logo from '../assets/imagens/'
+import Logo from '../assets/imagens/Logo_branco.png'
 import './styles/Cadastro.css'
 const Cadastro = () => {
     return (
         <div className="background">
-            <div className="Logo">
-            <img src="" alt="logo" />
+            <div >
+                <img className="Logo" src={Logo} alt="logo" />
             </div>
             <div className="form">
                 <form action="" className='Inputs'>
