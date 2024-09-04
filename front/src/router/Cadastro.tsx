bb936ef8bfed3b437c5f981d57e92e651b8426d3
mrBicarato <mr.bicarato@outlook.com>
2024-09-04 18:55:08 -0300

    continuação cadastro

--- front/src/router/Cadastro.tsx
+++ front/src/router/Cadastro.tsx
@@ -20,9 +20,40 @@ const Cadastro = () => {
             {/* ================================================================================= */}
 
             <div className="main_cadastro">
-                <div className="container_title">
-                    <h1></h1>
+                <div className="barra_porple"></div>
+                <div className="container_Cadastro">
+                    <h1 className="title_cadastro">Cadastre a sua Escola</h1>
+                    <br />
+                    <hr className="hr_linha" />
+                    <br />
+                    <h3 className="conteudo_container_cadastro">Apresentamos o STUDENTSCOOL, uma plataforma inovadora que nasceu com uma missão clara:
+                        combater o desperdício de alimentos nas escolas. A ideia inicial do site era permitir que os alunos visualizassem o cardápio escolar
+                        e indicassem suas intenções de consumo. Isso visava promover uma gestão mais eficiente dos recursos alimentares e reduzir o desperdício gerado pelas escolas.</h3>
                 </div>
+                {/* container de perguntas */}
+                <div className="barra_porple"></div>
+                <div className="container_Cadastro_itens">
+                    <label htmlFor="" className="conteudo_container_cadastro">Nome da escola</label>
+                    <input type="text"
+                        className="input_container"
+                        id="Nome_escola"
+                        placeholder="Nome da escola"
+                    />
+                    <hr className="hr_opacity" />
+                </div>
+
+                <div className="barra_porple"></div>
+                <div className="container_Cadastro_itens">
+                    <label htmlFor="" className="conteudo_container_cadastro">Nome da escola</label>
+                    <input type="text"
+                        className="input_container"
+                        id="Nome_escola"
+                        placeholder="Nome da escola"
+                    />
+                    <hr className="hr_opacity" />
+                </div>
+
+
             </div>
         </div>
     )
