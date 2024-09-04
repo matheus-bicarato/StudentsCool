510dce9d569149e4d79897f76998b0d1cc8d0a6f
Lucas Santos Gama <130692787+LucasSGama@users.noreply.github.com>
2024-09-03 23:57:37 -0300

    Adicionando verificações para os métodos do controller

--- back/src/main/java/com/studentscool/StudentsCool/adapters/in/web/ContatoController.java
+++ back/src/main/java/com/studentscool/StudentsCool/adapters/in/web/ContatoController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/comentarios")
@@ -20,48 +21,78 @@ public class ContatoController {
     @Autowired
     private ContatoUseCases contatoUseCases;
 
-    @PostMapping()
-    public Contato criarContato(@RequestBody Contato contato) {
-        Contato resultado = contatoUseCases.PostContato(contato);
-        return resultado;
+    public boolean isInvalidField(String field) {
+        return field == null || field.trim().isEmpty() || field.equals("null");
     }
 
+
+    @PostMapping()
+    public ResponseEntity<?> criarContato(@RequestBody Contato contato) {
+        try {
+            if (isInvalidField(contato.getEmail()) ||
+                    isInvalidField(contato.getMensagem()) ||
+                    isInvalidField(contato.getNome()) ||
+                    isInvalidField(contato.getTelefone())) {
+                return ResponseEntity.badRequest().body("Formulário com falta de informações necessárias.");
+            }
+
+            Contato resultado = contatoUseCases.PostContato(contato);
+            return ResponseEntity.ok(resultado);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body("Erro de requisição: " + e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro inesperado: " + e.getMessage());
+        }
+    }
+
+
     @GetMapping("/{id}")
-    public ResponseEntity<Contato> getContatoById(@PathVariable Long id) {
-        Contato contato = contatoUseCases.getComentarioById(id);
-        return ResponseEntity.ok().body(contato);
+    public ResponseEntity<?> getContatoById(@PathVariable(value = "id") Long id) {
+        try {
+            Contato contato = contatoUseCases.getComentarioById(id);
+            return ResponseEntity.status(HttpStatus.OK).body(contato);
+        } catch (NoSuchElementException e) {
+            Map<String, String> errorResponse = new HashMap<>();
+            errorResponse.put("Mensagem", e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+        } catch (Exception e) {
+            Map<String, String> errorResponse = new HashMap<>();
+            errorResponse.put("Mensagem", "Erro intero no servidor.");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+        }
     }
 
     @GetMapping()
-    public List<Contato> listar() {
-        return contatoUseCases.getAllComentarios();
+    public ResponseEntity<?> listar() {
+        List<Contato> contatos = contatoUseCases.getAllComentarios();
+
+        if (contatos.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+        } else {
+            return ResponseEntity.ok(contatos);
+        }
     }
+
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Map<String, String>> deleteComentario(@PathVariable(value = "id") Long id) {
         try {
-            // Busca todos os contatos
             List<Contato> todosContatos = contatoUseCases.getAllComentarios();
 
-            // Verifica se o ID está presente na lista de contatos
             boolean idExiste = todosContatos.stream().anyMatch(contato -> contato.getId().equals(id));
 
             if (!idExiste) {
-                // Retorna uma resposta 404 se o ID não for encontrado
                 Map<String, String> errorResponse = new HashMap<>();
-                errorResponse.put("message", "Contato com ID " + id + " não encontrado");
+                errorResponse.put("message", "Contato com ID " + id + " não foi encontrado");
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
             }
 
-            // Se o ID existe, deleta o contato
             contatoUseCases.DeletarComentario(id);
 
-            // Retorna uma resposta 200 após a deleção
             Map<String, String> response = new HashMap<>();
             response.put("message", "Contato com ID " + id + " foi deletado com sucesso");
             return ResponseEntity.ok(response);
         } catch (Exception e) {
-            // Captura exceções gerais e retorna uma resposta 500
             Map<String, String> errorResponse = new HashMap<>();
             errorResponse.put("message", "Erro interno do servidor: " + e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
