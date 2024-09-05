345fa4dc63a3620b37f69f8ce87882d486103eab
matheus-bicarato <mr.bicarato@outlook.com>
2024-09-05 15:22:21 -0300

    Error page

--- front/src/router/Error-page.tsx
+++ front/src/router/Error-page.tsx
@@ -1,10 +1,16 @@
 import './styles/Erro-page.css'
+import icon_error from '../assets/imagens/Error_Icon.png';
+import logo_error from '../assets/imagens/Logo_Retangulo.png'
+import { Link } from 'react-router-dom';
 
 const Error = () => {
-    return(
+    return (
         <div className="main_erro">
-            <h1 className='title_erro'>Error 440</h1>
-            <img src="" alt="" />
+            <h3 className='title_erro'>Error 404</h3>
+            <img src={icon_error} alt="icon_Error" className='icon_Error' />
+            <h1 className='subTitle_error'>Ops! Não encontamos <br /> essa página</h1>
+            <img src={logo_error} alt="logo_error" className='logo_error' />
+            <Link to={"/cadastro"}><button className="button_error_page">Voltar para a página inicial</button></Link>
         </div>
     )
 }
