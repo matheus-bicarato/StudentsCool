6a2be2ea9807c96d335ae614eb834526e7ab1b81
RichardCourtesy <130692757+RichardCourtesy@users.noreply.github.com>
2024-09-05 16:23:37 -0300

    landingPG fazendo

--- front/src/router/LandingPage.tsx
+++ front/src/router/LandingPage.tsx
@@ -1,10 +1,30 @@
+import Header_simplificado from "../components/Header-simplificado";
+import Footer from "../components/Footer";
+
+import Banner from "../assets/imagens/banner_landingPG.png";
+
+import './styles/LandingPage.css'
 
 const LandingPage = () => {
     return (
         <div>
+            <Header_simplificado />
             <main>
-                <h1>cu</h1>
+                <div className="Banner_LandPG">
+                    <img src={Banner} alt=""  />
+                </div>
+                <div className="Cards">
+                    <div className="Card_1">
+                        <div className="Title_icon_LandPG">
+                            <img src="" alt="" />
+                            <h1></h1>
+                        </div>
+                        <p></p>
+                    </div>
+                    
+                </div>
             </main>
+            <Footer />
         </div>
     )
 }
