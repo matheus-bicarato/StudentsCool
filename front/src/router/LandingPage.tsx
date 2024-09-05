fc72700a58f8985b9024eb7a822c9640c289fb4a
RichardCourtesy <130692757+RichardCourtesy@users.noreply.github.com>
2024-09-05 15:58:38 -0300

    LandingPage

---
+++ front/src/router/LandingPage.tsx
@@ -0,0 +1,12 @@
+
+const LandingPage = () => {
+    return (
+        <div>
+            <main>
+                <h1>cu</h1>
+            </main>
+        </div>
+    )
+}
+
+export default LandingPage;
