1b29835c2a00f14f0210d69909c6c1e74de2735a
RichardCourtesy <130692757+RichardCourtesy@users.noreply.github.com>
2024-09-05 21:37:16 -0300

    LandPG fazendo

--- front/src/router/LandingPage.tsx
+++ front/src/router/LandingPage.tsx
@@ -2,6 +2,13 @@ import Header_simplificado from "../components/Header-simplificado";
 import Footer from "../components/Footer";
 
 import Banner from "../assets/imagens/banner_landingPG.png";
+import Icon1 from "../assets/imagens/Icon_lang1.png"
+import Icon2 from "../assets/imagens/Icon_lang2.png"
+import Icon3 from "../assets/imagens/Icon_lang3.png"
+import Icon4 from "../assets/imagens/Icon_lang4.png"
+import Icon5 from "../assets/imagens/Icon_lang5.png"
+import Icon6 from "../assets/imagens/Icon_lang6.png"
+import Icon7 from "../assets/imagens/Icon_lang7.png"
 
 import './styles/LandingPage.css'
 
@@ -14,12 +21,18 @@ const LandingPage = () => {
                     <img src={Banner} alt=""  />
                 </div>
                 <div className="Cards">
-                    <div className="Card_1">
+                    <div className="Card">
                         <div className="Title_icon_LandPG">
-                            <img src="" alt="" />
-                            <h1></h1>
+                            <img src={Icon1} alt="" />
+                            <h1>Comunicação</h1>
                         </div>
-                        <p></p>
+                        <p>A comunicação entre alunos e
+                        professores é assegurada por 
+                        chats por turma, uma página de 
+                        notícias escolares e o acompanhamento
+                        do progresso acadêmico, que inclui 
+                        boletins para facilitar a organização 
+                        e o desempenho.</p>
                     </div>
                     
                 </div>
