8c5b28104a983a7372571edb557d0c5bc5ce85d2
RichardCourtesy <130692757+RichardCourtesy@users.noreply.github.com>
2024-09-06 16:34:25 -0300

    cu

--- front/src/router/LandingPage.tsx
+++ front/src/router/LandingPage.tsx
@@ -37,32 +37,85 @@ const LandingPage = () => {
                     
                     <div className="Card">
                         <div className="Title_icon_LandPG">
-                            <img src={Icon1} alt="" />
-                            <h1>Comunicação</h1>
+                            <img src={Icon2} alt="" />
+                            <h1>Gestão Escolar</h1>
                         </div>
-                        <p>A comunicação entre alunos e
-                        professores é assegurada por 
-                        chats por turma, uma página de 
-                        notícias escolares e o acompanhamento
-                        do progresso acadêmico, que inclui 
-                        boletins para facilitar a organização 
-                        e o desempenho.</p>
+                        <p>A gestão escolar organiza e coordena
+                            a escola, incluindo planejamento,
+                            administração de recursos e supervisão
+                            acadêmica, garantindo uma comunicação
+                            eficaz e a melhoria contínua.</p>
                     </div>
 
                     <div className="Card">
                         <div className="Title_icon_LandPG">
-                            <img src={Icon1} alt="" />
-                            <h1>Comunicação</h1>
+                            <img src={Icon3} alt="" />
+                            <h1>Sustentabilidade</h1>
                         </div>
-                        <p>A comunicação entre alunos e
-                        professores é assegurada por 
-                        chats por turma, uma página de 
-                        notícias escolares e o acompanhamento
-                        do progresso acadêmico, que inclui 
-                        boletins para facilitar a organização 
-                        e o desempenho.</p>
+                        <p>Escolas brasileiras enfrentam
+                            desperdício de alimentos devido
+                            à insatisfação com as refeições,
+                            comunicação ineficaz entre cantina
+                            e alunos, chamadas incorretas e
+                            desperdício de papel com impressões.</p>
                     </div>
-                    
+                </div>
+
+                <div className="Servico">
+                    <div className="Title_Servico_LangPG">
+                        <h1>Serviços</h1>
+                        <p>Conheça nosso  portfólio de serviços</p>
+                    </div>
+
+                    <div className="Servico_Card_LangPG">
+                        <div className="Title_icon_LandPG">
+                            <img src={Icon4} alt="" />
+                            <h1>Cardápio</h1>
+                        </div>
+                        <p>A tela exibirá o cardápio semanal
+                            e lanches diários. Alunos podem
+                            escolher e avaliar lanches. A cantina
+                            ajusta a produção e limita quantidades
+                            com base no feedback. O cardápio
+                            semanal é acessível apenas para
+                            quem confirmar presença.</p>
+                    </div>
+
+                    <div className="Servico_Card_LangPG">
+                        <div className="Title_icon_LandPG">
+                            <img src={Icon5} alt="" />
+                            <h1>Boletim</h1>
+                        </div>
+                        <p>O aluno poderá ver suas notas
+                            e média por matéria e receberá
+                            o boletim ao final de cada etapa,
+                            ajudando a monitorar o desempenho
+                            e identificar áreas com notas baixas.</p>
+                    </div>
+
+                    <div className="Servico_Card_LangPG">
+                        <div className="Title_icon_LandPG">
+                            <img src={Icon6} alt="" />
+                            <h1>Chat</h1>
+                        </div>
+                        <p>Os chats por turma permitirão que
+                            apenas os professores enviem mensagens,
+                            arquivos e atividades. Haverá um mural
+                            com datas importantes para ajudar os
+                            alunos a se organizarem e a não perderem
+                            conteúdo.</p>
+                    </div>
+
+                    <div className="Servico_Card_LangPG">
+                        <div className="Title_icon_LandPG">
+                            <img src={Icon7} alt="" />
+                            <h1>Notícias</h1>
+                        </div>
+                        <p>Uma página exclusiva para notícias escolares
+                            divulgará informações atualizadas, como
+                            eventos, projetos estudantis e mudanças
+                            no calendário</p>
+                    </div>
                 </div>
             </main>
             <Footer />
