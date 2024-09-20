74847f07aed89f4da1ac1ff5ae1ef7f3a5e831be
matheus-bicarato <mr.bicarato@outlook.com>
2024-09-20 09:20:15 -0300

    CheckList completo

--- front/src/components/checklist/Checkbox_list1.tsx
+++ front/src/components/checklist/Checkbox_list1.tsx
@@ -2,75 +2,53 @@ import React, { useState } from 'react';
 import '../styles/Checkbox.css';
 import Counter from './Count';
 
-const CheckboxList3 = () => {
-    const [selectedOptions, setSelectedOptions] = useState({});
-    const [itemCounts, setItemCounts] = useState({});
-
-    const items = [
-        {
-            id: 1, label: 'Pão integral com queijo branco 1 unidade(60 g)' },
-        { id: 2, label: 'Maçã 1 unidade (150 g)' },
-        { id: 3, label: 'Suco de laranja natural 300 ml' },
-    ];
+const CheckboxList = ({ title, items }) => {
+    const [itemsState, setItemsState] = useState({});
 
     const handleChange = (event) => {
         const { name, checked } = event.target;
-        setSelectedOptions((prevState) => ({
+        setItemsState((prevState) => ({
             ...prevState,
-            [name]: checked,
+            [name]: checked ? { count: 1 } : undefined,
         }));
-
-        if (checked) {
-            setItemCounts((prevState) => ({
-                ...prevState,
-                [name]: 1,
-            }));
-        } else {
-            setItemCounts((prevState) => {
-                const { [name]: removed, ...rest } = prevState;
-                return rest;
-            });
-        }
     };
 
     const handleCountChange = (name, increment) => {
-        setItemCounts((prevState) => {
-            const currentCount = prevState[name] || 0;
-            const newCount = Math.max(currentCount + increment, 0);
+        setItemsState((prevState) => {
+            const currentItem = prevState[name] || { count: 0 };
+            const newCount = Math.max(currentItem.count + increment, 0);
             return {
                 ...prevState,
-                [name]: Math.min(newCount, 3), // Define o máximo como 3
+                [name]: newCount > 0 ? { count: Math.min(newCount, 3) } : undefined,
             };
         });
     };
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log('Opções selecionadas:', selectedOptions);
-        console.log('Contagem de itens:', itemCounts);
+        console.log('Opções selecionadas:', itemsState);
     };
 
     return (
         <form className='form_container_filtro' onSubmit={handleSubmit}>
             <div className="barra_porple"></div>
             <div className="container_Filtro_itens">
-                <h1>Escolha seu Lanche</h1>
+                <h1>{title}</h1>
                 <div className="checkbox-container">
-                    <h1>Manhã</h1>
                     {items.map((item) => (
                         <div key={item.id} className='input_chacklist_filtro'>
                             <label>
                                 <input
                                     type="checkbox"
                                     name={`item${item.id}`}
-                                    checked={!!selectedOptions[`item${item.id}`]}
+                                    checked={!!itemsState[`item${item.id}`]}
                                     onChange={handleChange}
                                 />
                                 {item.label}
                             </label>
-                            {selectedOptions[`item${item.id}`] && (
+                            {itemsState[`item${item.id}`] && (
                                 <Counter
-                                    count={itemCounts[`item${item.id}`] || 0}
+                                    count={itemsState[`item${item.id}`].count}
                                     onIncrement={() => handleCountChange(`item${item.id}`, 1)}
                                     onDecrement={() => handleCountChange(`item${item.id}`, -1)}
                                 />
@@ -78,7 +56,6 @@ const CheckboxList3 = () => {
                         </div>
                     ))}
                 </div>
-                <hr className="hr_opacity" />
             </div>
             <div className="container_Filtro_button">
                 <button className="button_Filtro" type="submit">Enviar</button>
@@ -87,4 +64,4 @@ const CheckboxList3 = () => {
     );
 };
 
-export default CheckboxList3;
+export default CheckboxList;
--- front/src/router/Filtro_cardapio.tsx
+++ front/src/router/Filtro_cardapio.tsx
@@ -1,15 +1,34 @@
-import CheckboxList1 from '../components/checklist/Checkbox_list1';
+import CheckboxList from '../components/checklist/Checkbox_list1';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import './styles/filtro_cardapio.css'
 
 const Filtro_cardapio = () => {
+    const Manha = [
+        { id: 1, label: 'Pão integral com queijo branco 1 unidade(60 g)' },
+        { id: 2, label: 'Maçã 1 unidade (150 g)' },
+        { id: 3, label: 'Suco de laranja natural (300 ml)' },
+    ];
+
+    const Almoco = [
+        { id: 1, label: 'Arroz integral (250 g)' },
+        { id: 2, label: 'Frango grelhado 2 filés (250 g)' },
+        { id: 3, label: 'Brócolis a vapor (80 g)' },
+        { id: 4, label: 'Salada de cenoura com pepino (100 g)' },
+        { id: 5, label: 'Pera 1 unidade (130 g)' },
+    ];
+    const Tarde = [
+        { id: 1, label: 'Iogurte natural com granola (200 g) ' },
+        { id: 2, label: 'Biscoito integral (150 g)' },
+    ];
     return (
         <div className="">
             <Header />
             <div className="main_filtro_cardapio">
-                <h1 className='title_cardapio'>Veja o cardápio dessa <span className='span_cardapio'>Semana!</span></h1>
-                <CheckboxList1 />
+                <h1 className='title_cardapio'>Veja o cardápio de <span className='span_cardapio'>HOJE!</span></h1>
+                <CheckboxList title="Escolha seu Lanche da Manhã" items={Manha} />
+                <CheckboxList title="Escolha seu Almoço" items={Almoco} />
+                <CheckboxList title="Escolha seu Lanche da Tarde" items={Tarde} />
             </div>
             <Footer />
         </div>
