9d65b6ae5dc9c6a8416d56c9b7cf87fdf61b2334
Lucas Santos Gama <lucasantosgama3@gmail.com>
2024-09-26 16:16:20 -0300

    erros

--- front/src/router/Contato.tsx
+++ front/src/router/Contato.tsx
@@ -7,7 +7,7 @@ import Swal from 'sweetalert2';
 
 import ZAP from '../assets/imagens/Zap_Icon.png';
 import EMAIL from '../assets/imagens/Email_Icon.png';
-import  INSTA from '../assets/imagens/Insta_Black_Icon.png'
+import INSTA from '../assets/imagens/Insta_Black_Icon.png'
 import Retangulo from '../assets/imagens/Logo_Retangulo.png'
 
 import './styles/Contato.css'
@@ -17,7 +17,7 @@ const Contato = () => {
     const [email, setEmail] = useState('');
     const [telefone, setTelefone] = useState('');
     const [mensagem, setMensagem] = useState('');
-    
+
     const ContatoSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
@@ -40,7 +40,7 @@ const Contato = () => {
             setEmail('');
             setTelefone('');
             setMensagem('');
-            
+
             Swal.fire({
                 title: 'Sucesso!',
                 text: 'Mensagem enviada com sucesso!',
@@ -54,98 +54,94 @@ const Contato = () => {
     };
 
     return (
-    <div>
-        <Header/>
-        <main className='main_contato'>
-            <div className='Duvidas_cont_contato'>
-                <h1 className='Duvida_contato'>
-                    Alguma dúvida?
-                    <br />
-                    Entre em contato com a gente agora mesmo!
-                </h1>
-                
-                <div className="contatos">
-                    <ul className="contatosUl">
-                        
-                        <li><a href="">
-                            <span><img src={ZAP} alt="ZAP" /></span>
-                            <h1>(11) 97044-1052</h1>
-                        </a></li>
-                        
+        <div>
+            <Header />
+            <main className='main_contato'>
+                <div className='Duvidas_cont_contato'>
+                    <h1 className='Duvida_contato'>
+                        Alguma dúvida?
+                        <br />
+                        Entre em contato com a gente agora mesmo!
+                    </h1>
 
-                        <li><a href="" >
-                            <span><img src={EMAIL} alt="" /></span>
-                            <h1>studentscool@gmail.com</h1>
+                    <div className="contatos">
+                        <ul className="contatosUl">
+
+                            <li><a href="">
+                                <span><img src={ZAP} alt="ZAP" /></span>
+                                <h1>(11) 97044-1052</h1>
                             </a></li>
 
-                        <li><a href="">
-                        <span><img src={INSTA} alt="" /></span>
-                            <h1>@studentscool</h1>
-                        </a></li>
-                    </ul>
-                </div>
-            </div>
 
-<<<<<<< Updated upstream
-            <form className='Form_contato'>
-=======
-            <form className='Form_contado' onSubmit={ContatoSubmit}>
->>>>>>> Stashed changes
+                            <li><a href="" >
+                                <span><img src={EMAIL} alt="" /></span>
+                                <h1>studentscool@gmail.com</h1>
+                            </a></li>
 
-                <div className='Title_form_contato'>
-                    <span className='Contato_span'><h1>Nos envie uma mensagem</h1></span>
-                    <hr/>
+                            <li><a href="">
+                                <span><img src={INSTA} alt="" /></span>
+                                <h1>@studentscool</h1>
+                            </a></li>
+                        </ul>
+                    </div>
                 </div>
 
-                <div className='Inputs_form_contato'>
-                <input type="text"
-                        placeholder='Nome Completo'
-                        className='Nome_contato' 
-                        maxLength={300} 
-                        required
-                        value={nome}
-                        onChange={(e) => setNome(e.target.value)}
+                <form className='Form_contado' onSubmit={ContatoSubmit}>
+
+                    <div className='Title_form_contato'>
+                        <span className='Contato_span'><h1>Nos envie uma mensagem</h1></span>
+                        <hr />
+                    </div>
+
+                    <div className='Inputs_form_contato'>
+                        <input type="text"
+                            placeholder='Nome Completo'
+                            className='Nome_contato'
+                            maxLength={300}
+                            required
+                            value={nome}
+                            onChange={(e) => setNome(e.target.value)}
                         />
-                        
+
 
                         <div className='Email_tel_contato'>
-                            <input 
-                            type="email"
-                            id='email'
-                            placeholder='E-mail'
-                            className='Email_contato' 
-                            required
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
+                            <input
+                                type="email"
+                                id='email'
+                                placeholder='E-mail'
+                                className='Email_contato'
+                                required
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
                             />
 
                             <input type="tel"
-                            placeholder='Telefone'
-                            className='Tel_contato '
-                            maxLength={11} 
-                            required
-                            value={telefone}
-                            onChange={(e) => setTelefone(e.target.value)}
+                                placeholder='Telefone'
+                                className='Tel_contato '
+                                maxLength={11}
+                                required
+                                value={telefone}
+                                onChange={(e) => setTelefone(e.target.value)}
                             />
                         </div>
 
                         <textarea
-                                placeholder='Mensagem...'
-                                className='Mensagem_contato' 
-                                maxLength={300}
-                                required
-                                value={mensagem}
-                                onChange={(e) => setMensagem(e.target.value)}
-                                />
-                </div>
-                <img src={Retangulo} alt="" className='Logo_contato'/>
-                <button type="submit" className='Button_submit_contato'>Enviar</button>
-            </form>
-        </main>
-        <div className='Footer_contato'>
-            <Footer />
+                            placeholder='Mensagem...'
+                            className='Mensagem_contato'
+                            maxLength={300}
+                            required
+                            value={mensagem}
+                            onChange={(e) => setMensagem(e.target.value)}
+                        />
+                    </div>
+                    <img src={Retangulo} alt="" className='Logo_contato' />
+                    <button type="submit" className='Button_submit_contato'>Enviar</button>
+                </form>
+            </main>
+            <div className='Footer_contato'>
+                <Footer />
+            </div>
         </div>
-    </div>
     )
 }
 
