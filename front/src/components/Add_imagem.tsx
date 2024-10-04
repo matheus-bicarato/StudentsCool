0f603fd24c44e6f6c62e55067edef64f061e1528
matheus-bicarato <mr.bicarato@outlook.com>
2024-10-04 13:57:09 -0300

    arrumando erro de imagem e form

--- front/src/components/Add_imagem.tsx
+++ front/src/components/Add_imagem.tsx
@@ -1,4 +1,3 @@
-// ImageUpload.tsx
 import React, { useState } from 'react';
 import './styles/add_imagem.css'; // Importa o arquivo de estilo
 
@@ -18,22 +17,23 @@ const ImageUpload: React.FC = () => {
         }
     };
 
-    const handleUploadClick = () => {
-        // Função para lidar com o upload, se necessário
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault(); // Impede o comportamento padrão do form
+
         if (selectedImage) {
             console.log('Imagem selecionada:', selectedImage);
             // Aqui você pode adicionar a lógica para enviar a imagem ao servidor ou banco de dados
         }
     };
 
-    const handleRemoveImage = () => {
-        // Função para remover a imagem selecionada
+    const handleRemoveImage = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault(); // Impede comportamento padrão do botão
         setSelectedImage(null);
         setPreviewUrl(null);
     };
 
     return (
-        <form className="image-upload-container">
+        <form className="image-upload-container" onSubmit={handleSubmit}>
             <input
                 type="file"
                 accept="image/*"
@@ -42,6 +42,7 @@ const ImageUpload: React.FC = () => {
                 id="fileInput"
             />
             <button
+                type="button" // Muda o tipo do botão para evitar que submeta o form
                 className="upload-button"
                 onClick={() => document.getElementById('fileInput')?.click()}
             >
@@ -51,15 +52,19 @@ const ImageUpload: React.FC = () => {
             {previewUrl && (
                 <div className='container_preview'>
                     <img src={previewUrl} alt="Pré-visualização" className="image-preview" />
-                    <button className="remove-button button_padrao" onClick={handleRemoveImage}>
+                    <button
+                        type="button" // Define como botão comum para não submeter o form
+                        className="remove-button button_padrao"
+                        onClick={handleRemoveImage}
+                    >
                         Remover Imagem
                     </button>
                 </div>
             )}
 
             <button
+                type="submit" // Define o botão de submissão do form
                 className="upload-button"
-                onClick={handleUploadClick}
                 disabled={!selectedImage}
             >
                 Fazer Upload
